ec5e7b6a179233f35339dfb3a5b1a6cd2e417742
BrianStritch <brians12ca18@gmail.com>
2022-03-09 23:24:33 +0000

    add templates and css

--- reviews/models.py
+++ reviews/models.py
@@ -2,15 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-STATUS = ((0, 'Draft'),(1, 'Published'))
-
+STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 # Reviews Model
 class Review(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts') # noqa
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Review
 
-# Create your views here.
+class ReviewsList(generic.ListView):    
+    model = Review
+    queryset = Review.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+
