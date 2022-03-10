44f3f1966538b04a76e7d752d5a051cffbc96b15
BrianStritch <brians12ca18@gmail.com>
2022-03-10 00:25:54 +0000

    set up reviews page and view reviews

--- reviews/views.py
+++ reviews/views.py
@@ -5,7 +5,7 @@ from .models import Review
 class ReviewsList(generic.ListView):    
     model = Review
     queryset = Review.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    paginate_by = 6
+    template_name = 'reviews.html'
+    paginate_by = 8
 
 
